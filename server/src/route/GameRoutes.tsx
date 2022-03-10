76d713888eebd54e17dc9746d334c8621f843aa5
Livecan <roman.meszaros@gmail.com>
2022-03-10 15:37:09 +0200

    ADDED Games->Join

--- server/src/route/GameRoutes.tsx
+++ server/src/route/GameRoutes.tsx
@@ -1,32 +1,41 @@
 import express from "express";
 import { authenticate } from "../service/authentication.service";
-import { view } from "../service/game.service";
-import { NotFoundError } from "../utils/errors";
+import { view, join } from "../service/game.service";
+import { AuthenticationFailedError, NotFoundError } from "../utils/errors";
 
 const router = express.Router();
 
-router.route('/:gameId')
-  .get((req, res) => {
-    view(parseInt(req.params.gameId))
-      .then(response => res.send(response))
-      .catch(e => {
-        if (e instanceof NotFoundError) {
-          res.sendStatus(404).send("Game not found");
-        }
-        else {
-          res.sendStatus(500).send();
-          throw e;
-        }
-      });
-  });
+router.route("/:gameId").get((req, res) => {
+  view(parseInt(req.params.gameId))
+    .then((response) => res.send(response))
+    .catch((e) => {
+      if (e instanceof NotFoundError) {
+        res.sendStatus(404).send("Game not found");
+      } else {
+        res.sendStatus(500).send();
+        throw e;
+      }
+    });
+});
 
-router.route('/:gameId/join')
-  .post((req, res) => {
-    authenticate(req)
-      .then(user => {
-        console.log(user);
-        res.send(user);
-      });
-  })
+router.route("/:gameId/join").post((req, res) => {
+  authenticate(req)
+    .then((user) => {
+      join(req.params.gameId, user.id)
+        .then(() => res.send("Joined the game!"))
+        // @todo Figure out if want to do anything about duplicate entries
+        // here, currently does not respond here
+        .catch((e) => {
+          throw e;
+        });
+    })
+    .catch((e) => {
+      if (e instanceof AuthenticationFailedError) {
+        res.sendStatus(401).send("Authentication failed");
+      } else {
+        throw e;
+      }
+    });
+});
 
 export default router;
--- server/src/service/game.service.tsx
+++ server/src/service/game.service.tsx
@@ -1,15 +1,19 @@
-import sqlConnection from "./db.service";
-import { initModels, games } from "../models/init-models";
+import { games, gamesUsers } from "../models/init-models";
 import { NotFoundError } from "../utils/errors";
 
-const view = async (gameId : int) : Promise<Object> => {
+const view = async (gameId : number) : Promise<Object> => {
   // @todo Read about promise rejection problems here
   const game = await games.findByPk(gameId);
   if (game === null) {
     throw new NotFoundError();
   }
-console.log(game);
   return game;
 };
 
-export { view };
+const join = async (gameId : number, userId : number) : Promise<void> => {
+  const game = await games.findByPk(gameId);
+  const gameUser = gamesUsers.build({ gameId: gameId, userId: userId });
+  await gameUser.save();
+}
+
+export { view, join };
