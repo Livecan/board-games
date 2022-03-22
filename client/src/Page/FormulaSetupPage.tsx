c0c0d805c67363b81fcc884dad705fee2d0b4c22
Livecan <roman.meszaros@gmail.com>
2022-03-22 22:05:47 +0200

    ADDED Formula - Setup page - Preview

--- client/src/Page/FormulaSetupPage.tsx
+++ client/src/Page/FormulaSetupPage.tsx
@@ -3,7 +3,16 @@ import { useParams } from "react-router-dom";
 import useWebSocket from "../Hook/UseWebSocketHook";
 import commonConfig from "../../../common/src/config/config";
 import loginContext from "../Context/LoginContext";
-import { Box, Grid, Paper, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Grid,
+  MenuItem,
+  Paper,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { gamesAttributes } from "../../../common/src/models/generated/games";
 import { foCarsAttributes } from "../../../common/src/models/generated/foCars";
 import { gamesUsersAttributes } from "../../../common/src/models/generated/gamesUsers";
@@ -12,9 +21,33 @@ import { foDamages } from "../../../common/src/models/generated/foDamages";
 import { usersAttributes } from "../../../common/src/models/generated/users";
 
 interface fullGame extends gamesAttributes, foGamesAttributes {
-  foCars: (foCarsAttributes & {foDamages: foDamages[]})[];
-  gamesUsers: (gamesUsersAttributes & {user: usersAttributes})[];
-};
+  foCars: (foCarsAttributes & { foDamages: foDamages[] })[];
+  gamesUsers: (gamesUsersAttributes & { user: usersAttributes })[];
+}
+
+const FlexBox = ({ sx = {}, children = {}, ...rest }) => (
+  <Box
+    sx={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      ...sx,
+    }}
+    {...rest}
+  >
+    {children}
+  </Box>
+);
+
+const NumberTextField = ({ sx = {}, ...rest }) => (
+  <TextField
+    sx={{ margin: ".1rem", ...sx }}
+    inputProps={{ style: { textAlign: "center" } }}
+    style={{ width: "4rem" }}
+    {...rest}
+  />
+);
 
 const FormulaSetupPage: React.FC = () => {
   const [userData] = useContext(loginContext);
@@ -27,58 +60,93 @@ const FormulaSetupPage: React.FC = () => {
     `ws://localhost:5000/${commonConfig.apiBaseUrl}formula/${gameId}/setup`,
     (msg: fullGame) => setGame(msg),
     {
-      token: userData.jwt
+      token: userData.jwt,
     }
   );
 
+  const isCreator = userData?.user.id == game?.creatorId;
+
   return (
-    game != null &&
+    game != null && (
       <Grid container>
         <Grid item xs={12} padding={2}>
           <Typography variant="h3">{game.name}</Typography>
         </Grid>
         <Grid container item xs={12}>
           {/** Car damages panel */}
-          <Grid item xs={12} sm={8} padding={2}>
+          <Grid item xs={12} md={8} lg={9} padding={2}>
             <Stack spacing={2}>
-              {
-                game.gamesUsers.map((gameUser) =>
-                  <Paper key={gameUser.userId} elevation={4} sx={{padding: 2}}>
-                    <Typography variant="h6">{`${gameUser.user.name}'s cars`}</Typography>
-                    <Stack spacing={2} padding={2}>
-                    {
-                      game.foCars.filter(car => car.userId == gameUser.userId).slice(0, game.carsPerPlayer).map(car =>
-                        <Paper key={car.id} elevation={8} sx={{padding: 2, width: "fit-content"}}>
-                          <Box sx={{flexWrap: "wrap"}} >
-                          {
-                            car.foDamages.map(damage =>
-                              // @todo Use validation rules to avoid invalid numbers, validation test on backend
-                              // @todo Only allow the user to change his/her own cars' damages
-                              <TextField key={damage.id} defaultValue={damage.wearPoints} disabled={gameUser.userId != userData.user.id}
-                                sx={{margin: ".1rem"}}
-                                inputProps={{style: {textAlign: "center"}}} style={{width: "4rem"}} />
-                            )
-                          }
-                          </Box>
+              {game.gamesUsers.map((gameUser) => (
+                <Paper key={gameUser.userId} elevation={4} sx={{ padding: 2 }}>
+                  <Typography variant="h6">{`${gameUser.user.name}'s cars`}</Typography>
+                  <Stack spacing={2} padding={2}>
+                    {game.foCars
+                      .filter((car) => car.userId == gameUser.userId)
+                      .slice(0, game.carsPerPlayer)
+                      .map((car) => (
+                        <Paper
+                          key={car.id}
+                          elevation={8}
+                          sx={{ padding: 2, width: "fit-content" }}
+                        >
+                          <FlexBox
+                            sx={{
+                              justifyContent: "flexStart",
+                              flexWrap: "wrap",
+                            }}
+                          >
+                            {car.foDamages.map((damage) => (
+                              <React.Fragment>
+                                {/* @todo Use validation rules to avoid invalid numbers, validation test on backend */}
+                                {/* @todo Only allow the user to change his/her own cars' damages */}
+                                <NumberTextField
+                                  key={damage.id}
+                                  defaultValue={damage.wearPoints}
+                                  disabled={gameUser.userId != userData.user.id}
+                                />
+                              </React.Fragment>
+                            ))}
+                          </FlexBox>
                         </Paper>
-                      )
-                    }
-                    </Stack>
-                  </Paper>
-                )
-              }
+                      ))}
+                  </Stack>
+                </Paper>
+              ))}
             </Stack>
           </Grid>
           {/** Game setup panel */}
-          <Grid item xs={12} sm={4} padding={2}>
-            <Paper sx={{padding: 2}}>
+          <Grid item xs={12} md={4} lg={3} padding={2}>
+            <Paper elevation={4} sx={{ padding: 2 }}>
               <Stack spacing={2}>
-                <Typography>Game Setup Panel</Typography>
+                <Select defaultValue={game.foTrackId}>
+                  <MenuItem value={1}>Monaco</MenuItem>
+                </Select>
+                <FlexBox>
+                  <Typography display="inline" sx={{ flexGrow: 1 }}>
+                    Players
+                  </Typography>
+                  <NumberTextField placeholder="min" defaultValue={null} />
+                  <Typography display="inline">-</Typography>
+                  <NumberTextField placeholder="max" defaultValue={null} />
+                </FlexBox>
+                <FlexBox>
+                  <Typography display="inline">Laps</Typography>
+                  <NumberTextField defaultValue={game.laps} />
+                </FlexBox>
+                <FlexBox>
+                  <Typography display="inline">Wear Points</Typography>
+                  <NumberTextField defaultValue={game.wearPoints} />
+                </FlexBox>
+                <FlexBox>
+                  <Typography display="inline">Cars per player</Typography>
+                  <NumberTextField defaultValue={game.carsPerPlayer} />
+                </FlexBox>
               </Stack>
             </Paper>
           </Grid>
         </Grid>
       </Grid>
+    )
   );
 };
 
