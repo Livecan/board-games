5d44817e572220930728db3f80912993776e571c
Roman Meszaros <roman.meszaros@gmail.com>
2022-03-22 09:01:48 +0200

    CLEANUP Removal problem file
    Conflicting folder names on Windows

--- server/src/Route/FormulaGameRoutes.tsx
+++
@@ -1,7 +0,0 @@
-      const data = JSON.parse(msg);
-  (req: Request & { user: users }, res: Response) => {
-import { games } from "../../../common/src/models/generated/games";
-import { foCars } from "../../../common/src/models/generated/foCars";
-import { users, usersAttributes } from "../../../common/src/models/generated/users";
-import { foDamagesAttributes } from "../../../common/src/models/generated/foDamages";
-import { foDamagesAttributes } from "../../../common/src/models/generated/foDamages";
